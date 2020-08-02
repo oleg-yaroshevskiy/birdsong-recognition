cef420ca98d28b527f94ab39a078e588e634fd84
Oleg <oleg.yaroshevskiy@gmail.com>
2020-08-02 16:46:42 +0300

    seed + args

--- args.py
+++ args.py
@@ -1,23 +1,31 @@
-class args:
+import argparse
+import os
+import numpy as np
 
-    ROOT_PATH = "../input/train_audio"
+parser = argparse.ArgumentParser()
+parser.add_argument("--ROOT_PATH", type=str, default="../input/train_audio")
+parser.add_argument("--seed", type=int, default=42)
+parser.add_argument("--folds", type=int, default=5)
+parser.add_argument("--num_workers", type=int, default=16)
 
-    num_classes = 264
-    max_duration = 10  # seconds
+parser.add_argument("--model", type=str, default="se50")
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--epochs", type=int, default=30)
 
-    sample_rate = 32000
+parser.add_argument("--max_duration", type=int, default=10)
+parser.add_argument("--lr", type=float, default=0.0009)
+parser.add_argument("--wd", type=float, default=1e-5)
+parser.add_argument("--momentum", type=float, default=0.9)
+parser.add_argument("--eps", type=float, default=1e-8)
 
-    img_height = 128
-    img_width = 313
+parser.add_argument("--melspectrogram_parameters_n_mels", type=int, default=128)
+parser.add_argument("--melspectrogram_parameters_fmin", type=int, default=20)
+parser.add_argument("--melspectrogram_parameters_fmax", type=int, default=16000)
 
-    batch_size = 64
-    num_workers = 16
-    epochs = 30
+args = parser.parse_args()
+print("Initial arguments", args)
 
-    lr = 0.0009
-    wd = 1e-5
-    momentum = 0.9
-    eps = 1e-8
-    betas = (0.9, 0.999)
-
-    melspectrogram_parameters = {"n_mels": 128, "fmin": 20, "fmax": 16000}
+args.__dict__["betas"] = (0.9, 0.999)
+args.__dict__["num_classes"] = 264
+args.__dict__["sample_rate"] = 32000
+args.__dict__["melspectrogram_parameters"] = {"n_mels": 128, "fmin": 20, "fmax": 16000}
--- train.py
+++ train.py
@@ -4,16 +4,25 @@ from torch.utils.data import DataLoader
 from dataset import BirdDataset
 from args import args
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from neural import ResNet18
 from loops import train_fn, valid_fn
 import wandb
+import random
 
 # TODO: add seeds
 # TODO: pass config explicitly
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
+
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
 
 train = pd.read_csv("../input/train.csv")
 test = pd.read_csv("../input/test.csv")
@@ -27,10 +36,10 @@ for fold, (t_idx, v_idx) in enumerate(
     kfold.split(train.filename.values, train.ebird_code.values)
 ):
     wandb.init(
-        # config=args.__dict__,
+        config=args,
         project="birdsong",
-        name="resnet18_f{}".format(fold),
-        id="resnet18_f{}".format(fold),
+        name="{}_f{}".format(args.model, fold),
+        id="{}_f{}".format(args.model, fold),
     )
     train_df = train.loc[t_idx]
     train_dataset = BirdDataset(df=train_df)
@@ -43,8 +52,9 @@ for fold, (t_idx, v_idx) in enumerate(
         batch_size=args.batch_size,
         shuffle=True,
         pin_memory=True,
-        drop_last=False,
+        drop_last=True,
         num_workers=args.num_workers,
+        worker_init_fn=lambda _: np.random.seed(torch.initial_seed() % 2**32)
     )
 
     valid_loader = DataLoader(
@@ -53,10 +63,18 @@ for fold, (t_idx, v_idx) in enumerate(
         pin_memory=True,
         drop_last=False,
         num_workers=args.num_workers,
+        worker_init_fn=lambda _: np.random.seed(torch.initial_seed() % 2**32),
     )
 
     loss_fn = nn.CrossEntropyLoss(ignore_index=-1)
-    model = ResNet18(pretrained=False).to(device)
+    #model = ResNet18(pretrained=False).to(device)
+    model = torch.hub.load(
+        'rwightman/gen-efficientnet-pytorch', 
+        'tf_efficientnet_%s_ns' % args.model, 
+        pretrained=True,
+        num_classes = args.num_classes
+    ).cuda()
+
     optimizer = torch.optim.AdamW(
         model.parameters(),
         lr=args.lr,
